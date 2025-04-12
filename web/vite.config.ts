7f5427a3467839cbd8b8a28f4f6af5833f156f91
Bennie Rosas <ben@aliencyb.org>
2025-04-11 22:35:20 -0500

    web-code rule; zrok host

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+	plugins: [solid()],
+	server: {
+		allowedHosts: ["aliencyborg.share.zrok.io", "localhost"],
+	},
+});
