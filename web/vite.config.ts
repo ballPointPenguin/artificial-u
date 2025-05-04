65b823d01c9ca09824752957bcc4a8a5c4505a55
Bennie Rosas <ben@aliencyb.org>
2025-05-04 00:07:13 -0500

    install postcss

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [solid(), tailwindcss()],
   server: {
     allowedHosts: ['aliencyborg.share.zrok.io', 'localhost'],
+  },
+  build: {
+    target: 'esnext',
   },
 })
